a4c382ff0e4d16b1786709edbe25eda6a59053cf
sunshower76 <38250239+sunshower76@users.noreply.github.com>
2021-10-13 23:45:31 +0900

    Update densecl.py

--- openselfsup/models/densecl.py
+++ openselfsup/models/densecl.py
@@ -25,25 +25,37 @@ class DenseCL(nn.Module):
                  loss_lambda=0.5,
                  **kwargs):
         super(DenseCL, self).__init__()
+        # build함수에 따라, backbone, neck의 initializae된 클래스가 반환되며, sequential로 묶인다.
         self.encoder_q = nn.Sequential(
             builder.build_backbone(backbone), builder.build_neck(neck))
         self.encoder_k = nn.Sequential(
             builder.build_backbone(backbone), builder.build_neck(neck))
+        
         self.backbone = self.encoder_q[0]
+        # key encoder는 momentum으로 update되기 때문에 requires_grad=False로 만들어 학습이 되지않게 한다.
         for param in self.encoder_k.parameters():
             param.requires_grad = False
+            
+        # contrastive loss head.
+        # models/heads/contrastive_head.py
         self.head = builder.build_head(head)
+        
+        # weight initialization
         self.init_weights(pretrained=pretrained)
 
         self.queue_len = queue_len
         self.momentum = momentum
+        
+        # gloabl과 pixel level contrastive loss의 비율.
         self.loss_lambda = loss_lambda
 
         # create the queue
+        # register_buffer: 모델의 구성 ex)layer로써 존재하지만 update되지 않음.
         self.register_buffer("queue", torch.randn(feat_dim, queue_len))
         self.queue = nn.functional.normalize(self.queue, dim=0)
         self.register_buffer("queue_ptr", torch.zeros(1, dtype=torch.long))
-        # create the second queue for dense output
+        
+        # create the second queue for dense output/ dense key에 대한 dictionary도 만들어야함
         self.register_buffer("queue2", torch.randn(feat_dim, queue_len))
         self.queue2 = nn.functional.normalize(self.queue2, dim=0)
         self.register_buffer("queue2_ptr", torch.zeros(1, dtype=torch.long))
@@ -53,6 +65,8 @@ class DenseCL(nn.Module):
             print_log('load model from: {}'.format(pretrained), logger='root')
         self.encoder_q[0].init_weights(pretrained=pretrained)
         self.encoder_q[1].init_weights(init_linear='kaiming')
+        
+        # queue의 paramter를 그대로 key의 parameter에 복사한다.
         for param_q, param_k in zip(self.encoder_q.parameters(),
                                     self.encoder_k.parameters()):
             param_k.data.copy_(param_q.data)
@@ -70,14 +84,18 @@ class DenseCL(nn.Module):
     @torch.no_grad()
     def _dequeue_and_enqueue(self, keys):
         # gather keys before updating queue
+        
+        # 모든 프로세스에서 key를 모음.
         keys = concat_all_gather(keys)
 
         batch_size = keys.shape[0]
-
+        
+        # 현재 포인터가 가르키는곳.
         ptr = int(self.queue_ptr)
         assert self.queue_len % batch_size == 0  # for simplicity
 
         # replace the keys at ptr (dequeue and enqueue)
+        # 포인터부터 batchsize만큼 enqueue, dequeue과정 진행.
         self.queue[:, ptr:ptr + batch_size] = keys.transpose(0, 1)
         ptr = (ptr + batch_size) % self.queue_len  # move pointer
 
@@ -101,36 +119,43 @@ class DenseCL(nn.Module):
 
     @torch.no_grad()
     def _batch_shuffle_ddp(self, x):
-        """
-        Batch shuffle, for making use of BatchNorm.
+        """Batch shuffle, for making use of BatchNorm.
+
         *** Only support DistributedDataParallel (DDP) model. ***
         """
         # gather from all gpus
         batch_size_this = x.shape[0]
+        
+        # 모든 프로세스들에서 텐서를 모음.
         x_gather = concat_all_gather(x)
         batch_size_all = x_gather.shape[0]
 
         num_gpus = batch_size_all // batch_size_this
 
         # random shuffle index
+        # 총 배치수를 max index로 설정하여 random permutation하여 index를 섞음
         idx_shuffle = torch.randperm(batch_size_all).cuda()
 
         # broadcast to all gpus
+        # 섞은 index를 다시 분산된 환경에 복사시킴.
         torch.distributed.broadcast(idx_shuffle, src=0)
 
         # index for restoring
+        # index를 unshuffle하기위한 인덱스를 저장해둠.
         idx_unshuffle = torch.argsort(idx_shuffle)
 
         # shuffled index for this gpu
         gpu_idx = torch.distributed.get_rank()
+        
+        # 분산된 환경에 permutation된 key index를 해당 gpu에 배당된 개수만큼 분배.
         idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
 
         return x_gather[idx_this], idx_unshuffle
 
     @torch.no_grad()
     def _batch_unshuffle_ddp(self, x, idx_unshuffle):
-        """
-        Undo batch shuffle.
+        """Undo batch shuffle.
+
         *** Only support DistributedDataParallel (DDP) model. ***
         """
         # gather from all gpus
@@ -147,13 +172,24 @@ class DenseCL(nn.Module):
         return x_gather[idx_this]
 
     def forward_train(self, img, **kwargs):
+        """Forward computation during training.
+
+        Args:
+            img (Tensor): Input of two concatenated images of shape (N, 2, C, H, W).
+                Typically these should be mean centered and std scaled.
+            여기서 N은 배치, 2는 augmented query, key의 개수를 표시한다.
+
+        Returns:
+            dict[str, Tensor]: A dictionary of loss components.
+        """
         assert img.dim() == 5, \
             "Input must have 5 dims, got: {}".format(img.dim())
         im_q = img[:, 0, ...].contiguous()
         im_k = img[:, 1, ...].contiguous()
+        
         # compute query features
         q_b = self.encoder_q[0](im_q) # backbone features
-        q, q_grid, q2 = self.encoder_q[1](q_b)  # queries: NxC; NxCxS^2
+        q, q_grid, q2 = self.encoder_q[1](q_b)  # queries: NxC:gloabl; NxCxS^2:dense
         q_b = q_b[0]
         q_b = q_b.view(q_b.size(0), q_b.size(1), -1)
 
@@ -164,11 +200,14 @@ class DenseCL(nn.Module):
 
         # compute key features
         with torch.no_grad():  # no gradient to keys
+            # key encoder update 부분
             self._momentum_update_key_encoder()  # update the key encoder
 
             # shuffle for making use of BN
+            # shuffling 후,
             im_k, idx_unshuffle = self._batch_shuffle_ddp(im_k)
-
+            
+            # key를 통과시켜 key feature를 뽑음.
             k_b = self.encoder_k[0](im_k)
             k, k_grid, k2 = self.encoder_k[1](k_b)  # keys: NxC; NxCxS^2
             k_b = k_b[0]
@@ -186,12 +225,16 @@ class DenseCL(nn.Module):
             k_b = self._batch_unshuffle_ddp(k_b, idx_unshuffle)
 
         # compute logits
+        """ gloabl part """
         # Einstein sum is more intuitive
-        # positive logits: Nx1
+        # positive logits: Nx1 (c=128), inner product. get similarity.
         l_pos = torch.einsum('nc,nc->n', [q, k]).unsqueeze(-1)
-        # negative logits: NxK
+        
+        # negative logits: NxK, inner product. all corresponding between 'n' set in the batch and 'k' set in the dictionary.
+        # negative는 queue에서 복재해서 similarity를 구한다.
         l_neg = torch.einsum('nc,ck->nk', [q, self.queue.clone().detach()])
-
+        
+        """ dense part """
         # feat point set sim
         backbone_sim_matrix = torch.matmul(q_b.permute(0, 2, 1), k_b)
         densecl_sim_ind = backbone_sim_matrix.max(dim=2)[1] # NxS^2
@@ -205,14 +248,16 @@ class DenseCL(nn.Module):
         q_grid = q_grid.reshape(-1, q_grid.size(2))
         l_neg_dense = torch.einsum('nc,ck->nk', [q_grid,
                                             self.queue2.clone().detach()])
-
+        
+        # calculate contrastive loss.
         loss_single = self.head(l_pos, l_neg)['loss_contra']
         loss_dense = self.head(l_pos_dense, l_neg_dense)['loss_contra']
 
         losses = dict()
         losses['loss_contra_single'] = loss_single * (1 - self.loss_lambda)
         losses['loss_contra_dense'] = loss_dense * self.loss_lambda
-
+        
+        # dequeue and enqueue
         self._dequeue_and_enqueue(k)
         self._dequeue_and_enqueue2(k2)
 
@@ -241,15 +286,18 @@ class DenseCL(nn.Module):
 # utils
 @torch.no_grad()
 def concat_all_gather(tensor):
-    """
-    Performs all_gather operation on the provided tensors.
+    """Performs all_gather operation on the provided tensors.
+
     *** Warning ***: torch.distributed.all_gather has no gradient.
     """
+    # 각 분산된 환경에 있는 key tensor의 shape과 같은 one tensor를 모아두는 리스트 생성.
     tensors_gather = [
         torch.ones_like(tensor)
         for _ in range(torch.distributed.get_world_size())
     ]
+    # 모든 parallel process에 존재하는 key의 텐서를 가져와서 복제 
     torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
-
+    
+    # key tensor들을 concat 
     output = torch.cat(tensors_gather, dim=0)
     return output
